0838e579ce20e8217f321a84bf5e6408ab301b0f
KeresztesIulia <101juliakeresztes@gmail.com>
2023-10-29 10:53:56 +0100

    10. 29.

--- ULA7Z2_1025/SaxULA7Z2/src/hu/saxula7z2/SaxULA7Z2.java
+++ ULA7Z2_1025/SaxULA7Z2/src/hu/saxula7z2/SaxULA7Z2.java
@@ -8,15 +8,20 @@ import javax.xml.parsers.SAXParserFactory;
 
 public class SaxULA7Z2 {
 	   public static void main(String[] args){
-
-	      try {	
-	         File xml = new File("ULA7Z2_1025\\SaxULA7Z2\\ULA7Z2_kurzusfelvetel.xml");
-	         SAXParserFactory spFactory = SAXParserFactory.newInstance();
-	         SAXParser parser = spFactory.newSAXParser();
-	         SaxHandlerULA7Z2 userhandler = new SaxHandlerULA7Z2();
-	         parser.parse(xml, userhandler);     
-	      } catch (Exception e) {
-	         e.printStackTrace();
-	      }
+	     	try {	
+	         	File xml = new File("ULA7Z2_1025\\SaxULA7Z2\\ULA7Z2_kurzusfelvetel.xml");
+	         	SAXParserFactory spFactory = SAXParserFactory.newInstance();
+	         	SAXParser parser = spFactory.newSAXParser();
+	         	SaxHandlerULA7Z2 userhandler = new SaxHandlerULA7Z2();
+	         	parser.parse(xml, userhandler);     
+	     	} catch (IOException e) {
+	         	System.out.println("File megnyitása sikertelen!");
+				e.printStackTrace();
+	      	} catch (ParserConfigurationException e){
+				System.out.println("SAXParser létrehozási hiba!");
+				e.printStackTrace();
+		  	} catch (Exception e){
+				e.printStackTrace();
+		  	}
 	   }   
 	}
